1ea44ff581afdade64104f4285a759166ec7abe2
aaronvictor <aaron.victor30@gmail.com>
2022-06-12 23:03:21 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,59 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String input;
+
+
+        System.out.println("Select your party affiliation");
+
+        System.out.println("D for Democrat");
+
+        System.out.println("R for Republican");
+
+        System.out.println("I for Independent");
+
+        System.out.print("Enter your choice : ");
+
+
+        char c;
+
+        if (input.length() != 0) {
+
+            c = input.charAt(0);
+
+        } else {
+
+
+        }
+    }
+
+    c = 'O';
+
+}
+
+if(c=='D'){
+
+        System.out.println("You get a Democratic Donkey.");
+
+        }else if(c=='R'){
+
+        System.out.println("You get a Republican Elephant.");
+
+        }else if(c=='P'){
+
+        System.out.println("You get a Independent Person.");
+
+        }else{
+
+        System.out.println("You get an Other");
+
+        }
+
+        }
+
+        }
+        }
+        }
